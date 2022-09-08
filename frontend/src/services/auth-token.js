9b43fa7bd5c1df3d8f435daace1551bd16fc43ba
Mihir Patel <mihir.patel@neebal.com>
2022-09-08 17:57:13 +0530

    get pull

---
+++ frontend/src/services/auth-token.js
@@ -0,0 +1,12 @@
+// export default function authHeader() {
+//   const accessToken = JSON.parse(localStorage.getItem('accessToken'));
+//   if (accessToken) {
+//     // for Node.js Express back-end
+//     return {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${accessToken}`,
+//     };
+//   } else {
+//     return {};
+//   }
+// }
