889fb209feda629988068cde1e9a06fd71928f81
Divyang Radadiya <divyang.thinktanker@gmail.com>
2022-09-27 10:52:59 +0530

    date 9/27/2022

--- frontend/src/views/components/products/ProductsDetail.js
+++ frontend/src/views/components/products/ProductsDetail.js
@@ -1,59 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import { Card, CardBody, CardHeader, CardTitle, Col, Row } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { ProductsDetialRequest } from '../../../redux/productsSlice';
 import Breadcrumbs from '@components/breadcrumbs';
-import SwiperCore, { Thumbs } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Card, CardBody, CardHeader, CardTitle } from 'reactstrap';
-import { useRTL } from '@hooks/useRTL';
-import img1 from '@src/assets/images/banner/banner-11.jpg';
-import img2 from '@src/assets/images/banner/banner-12.jpg';
-import img3 from '@src/assets/images/banner/banner-13.jpg';
-import img4 from '@src/assets/images/banner/banner-14.jpg';
-import img5 from '@src/assets/images/banner/banner-15.jpg';
-
-import '@styles/react/libs/swiper/swiper.scss';
-
-SwiperCore.use([Thumbs]);
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/scss/image-gallery.scss';
 
 const ProductsDetail = () => {
 	const dispatch = useDispatch();
 	const { id } = useParams();
 
 	const [index, setIndex] = useState(0);
-	const [thumbsSwiper, setThumbsSwiper] = useState(null);
-	const [isRtl, setIsRtl] = useRTL();
 
 	const { productViewData, error } = useSelector((state) => state.products);
-	console.log(productViewData, 'productViewData');
+	// console.log(productViewData, 'productViewData');
 	useEffect(() => {
 		dispatch(ProductsDetialRequest(id));
 	}, []);
 
-	const handleTab = (index) => {
-		setIndex(index);
-	};
-
-	const params = {
-		className: 'swiper-gallery',
-		spaceBetween: 10,
-		navigation: true,
-		pagination: {
-			clickable: true,
-		},
-		thumbs: { swiper: thumbsSwiper },
-	};
-
-	const paramsThumbs = {
-		className: 'gallery-thumbs',
-		spaceBetween: 10,
-		slidesPerView: 4,
-		freeMode: true,
-		watchSlidesVisibility: true,
-		watchSlidesProgress: true,
-		onSwiper: setThumbsSwiper,
-	};
+	var getAllImages = [];
+	const getImages =
+		productViewData &&
+		productViewData?.images?.map((image) => {
+			var images = {
+				original: image,
+				thumbnail: image,
+			};
+			return getAllImages.push(images);
+		});
 
 	return (
 		<>
@@ -68,62 +43,18 @@ const ProductsDetail = () => {
 				</CardHeader>
 				{productViewData && productViewData?.images?.length > 0 ? (
 					<CardBody>
-						<div className="swiper-gallery">
-							<Swiper dir={isRtl ? 'rtl' : 'ltr'} {...params}>
-								{productViewData &&
-									productViewData?.images?.map((image, index) => {
-										return (
-											<SwiperSlide>
-												<img
-													src={image}
-													alt="swiper 3"
-													style={{ height: '350px', width: '100%' }}
-													className="img-fluid"
-												/>
-											</SwiperSlide>
-										);
-									})}
-
-								{/* <SwiperSlide>
-									<img src={img1} alt="swiper 1" className="img-fluid" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src={img2} alt="swiper 2" className="img-fluid" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src={img3} alt="swiper 3" className="img-fluid" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src={img4} alt="swiper 4" className="img-fluid" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src={img5} alt="swiper 5" className="img-fluid" />
-								</SwiperSlide> */}
-							</Swiper>
-							<Swiper {...paramsThumbs}>
-								{productViewData &&
-									productViewData?.images?.map((image, index) => {
-										return (
-											<SwiperSlide>
-												<img
-													src={image}
-													alt="swiper 3"
-													style={{ height: '100px', width: '100%' }}
-													className="img-fluid"
-												/>
-											</SwiperSlide>
-										);
-									})}
-
-								{/* 							
-								<SwiperSlide>
-									<img src={img4} alt="swiper 4" className="img-fluid" />
-								</SwiperSlide>
-								<SwiperSlide>
-									<img src={img5} alt="swiper 5" className="img-fluid" />
-								</SwiperSlide> */}
-							</Swiper>
-						</div>
+						<Row>
+							<Col xl="6">
+								<div className="image_slider">
+									<ImageGallery
+										items={getAllImages}
+										showPlayButton={false}
+										// showFullscreenButton={false}
+									/>
+									;
+								</div>
+							</Col>
+						</Row>
 					</CardBody>
 				) : (
 					'Images Not Found'
