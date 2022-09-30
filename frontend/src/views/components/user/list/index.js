0f05d3ea95bf1a9aac0c265c628c37289212d34e
dileep2malviya <dileep2malviya@gmail.com>
2022-09-30 10:48:19 +0530

    changes

---
+++ frontend/src/views/components/user/list/index.js
@@ -0,0 +1,16 @@
+// ** User List Component
+import Table from './Table';
+
+// ** Styles
+import '@styles/react/apps/app-users.scss';
+
+const FtpList = () => {
+	return (
+		<div className="app-user-list">
+			{/* <h1>FTP List</h1> */}
+			<Table />
+		</div>
+	);
+};
+
+export default FtpList;
