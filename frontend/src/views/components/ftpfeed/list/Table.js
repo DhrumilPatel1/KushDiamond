a51c4bd9d587f9f2eee68de3579f39ae7bb8c76a
divyang.thinktanker <divyang.thinktanker@gmail.com>
2023-03-20 14:05:30 +0530

    date 20-03-2023

--- frontend/src/views/components/ftpfeed/list/Table.js
+++ frontend/src/views/components/ftpfeed/list/Table.js
@@ -23,7 +23,7 @@ import {
 } from 'reactstrap';
 import '@styles/react/libs/react-select/_react-select.scss';
 import '@styles/react/libs/tables/react-dataTable-component.scss';
-import { sendFeed } from '../../../../redux/productsSlice';
+import { productList, sendFeed } from '../../../../redux/productsSlice';
 import { datatable_per_page, datatable_per_raw } from '../../../../configs/constant_array';
 import { selectThemeColors } from '@utils';
 import Swal from 'sweetalert2';
@@ -66,8 +66,12 @@ const FtpFeedList = () => {
 	const dispatch = useDispatch();
 	const { ftpGetAllData, ftpFeedData } = useSelector((state) => state.Ftps);
 
+	// const { productData } = useSelector((state) => state.products);
+
 	const getAllDropdownValue = ftpGetAllData.ftp_data?.map((item) => item);
+
 	const colorDropDownValue = ftpGetAllData.product_color?.map((item) => item);
+
 	const cutDropDownValue = ftpGetAllData.product_cut?.map((item) => item);
 
 	const shapeDropDownValue = ftpGetAllData.product_shape?.map((item) => item);
@@ -75,36 +79,63 @@ const FtpFeedList = () => {
 	const [limit, setPerPage] = useState(datatable_per_page);
 
 	const [sort_order, setSort_order] = useState('desc');
-	const [filterColor, setFilterColor] = useState('');
+	const [filterColor, setFilterColor] = useState([]);
+	const [filterShape, setFilterShape] = useState([]);
+	const [filterCut, setFilterCut] = useState([]);
+	const [colorLabelArray, setColorLabelArray] = useState([]);
 
-	const [filterShape, setFilterShape] = useState('');
-	const [filterCut, setFilterCut] = useState('');
-	const [ftpvalue, setFtpValue] = useState([]);
+	const [shapLabelArray, setShapeLabelArray] = useState([]);
+	const [cutLabelArray, setCutLabelArray] = useState([]);
+	const [selectedData, setSelectedData] = useState();
+
+	const [ftpvalue, setFtpValue] = useState();
+
+	const [array, setArray] = useState([]);
+
+	const [showTable, setShowTable] = useState(false);
 
 	const table_data = {
 		page: 1,
 		per_page: limit,
 		sort_order: sort_order,
-		color: filterColor.label == undefined ? '' : filterColor.label,
-		shape: filterShape.label == undefined ? '' : filterShape.label,
-		cut: filterCut.label == undefined ? '' : filterCut.label,
+		color__in: filterColor == undefined ? '' : colorLabelArray,
+		shape__in: filterShape == undefined ? '' : shapLabelArray,
+		cut__in: filterCut == undefined ? '' : cutLabelArray,
 		order_column: 'created_at',
 	};
 
 	const [queryString, setQueryString] = useState(
-		`page=${table_data.page}&color=${table_data.color}&shape=${table_data.shape}&cut=${table_data.cut}&per_page=${table_data.per_page}&order_column=${table_data.order_column}`
+		`page=${table_data.page}&color__in=${table_data.color__in}&shape__in=${table_data.shape__in}&cut__in=${table_data.cut__in}&per_page=${table_data.per_page}&order_column=${table_data.order_column}`
 	);
-
-	const ftpParams = {
-		fTPSting: `color=${filterColor.label == undefined ? '' : filterColor.label}&shape=${
-			filterShape.label == undefined ? '' : filterShape.label
-		}&cut=${filterCut.label == undefined ? '' : filterCut.label}`,
-	};
 
 	useEffect(() => {
 		dispatch(FtpGetDataDrowpDown());
+	}, []);
+
+	useEffect(() => {
 		dispatch(FtpFeedRecordList(queryString));
 	}, [dispatch, queryString]);
+
+	useEffect(() => {
+		if (ftpFeedData?.results) {
+			setSelectedData(ftpFeedData?.results);
+		}
+	}, [ftpFeedData]);
+
+	useEffect(() => {
+		if (array) {
+			setShowTable(false);
+			setArray(array);
+		}
+	}, [array.length]);
+
+	// useEffect(() => {
+	// 	dispatch(FtpGetDataDrowpDown());
+	// 	dispatch(productList(queryString));
+	// 	if (ftpFeedData?.results) {
+	// 		setSelectedData(ftpFeedData?.results);
+	// 	}
+	// }, [dispatch, queryString,ftpFeedData]);
 
 	const handlePerRowsChange = (newPerPage, page) => {
 		setPerPage(newPerPage);
@@ -126,16 +157,6 @@ const FtpFeedList = () => {
 
 	const filterSubmit = (e) => {
 		e.preventDefault();
-		// console.log(filterColor,"e.target.color.value")
-		// setFilterColor(e.target.color.value);
-		// setFilterShape(e.target.shape.value);
-		// setFilterCut(e.target.cut.value);
-		// tableChangeHandler({
-		// 	...table_data,
-		// 	color: e.target.color.value,
-		// 	shape: e.target.shape.value,
-		// 	cut: e.target.cut.value,
-		// });
 	};
 
 	const handleChange = (e) => {
@@ -145,7 +166,7 @@ const FtpFeedList = () => {
 	const openPopup = () => {
 		OpenSwal.fire({
 			title: 'Are you sure?',
-			text: `You want to feed these ${ftpFeedData?.count} product in FTP feed.`,
+			text: `You want to feed these ${array?.length > 0 ? array?.length : ftpFeedData?.count} product in FTP feed.`,
 			icon: 'warning',
 			showCancelButton: true,
 			confirmButtonText: 'Send Feed',
@@ -156,56 +177,84 @@ const FtpFeedList = () => {
 			buttonsStyling: false,
 		}).then((res) => {
 			if (res && res.isConfirmed) {
-				let ftpValues = ftpvalue.map((ele) => ele.value);
-				let ftpValuePass = {
-					ftp: ftpValues,
-				};
+				let ftpValues = ftpvalue?.map((ele) => ele.value);
+				let getSku = array?.map((ele) => ele.sku);
+				if (getSku?.length > 0) {
+					let ftpValuePass = {
+						ftp: ftpValues,
+						sku_list: getSku,
+					};
 
+					setFtpValue([]);
+					// dispatch(sendFeed(ftpValuePass));
+				} else {
+					let ftpValuePass = {
+						ftp: ftpValues,
+						sku_list: [],
+					};
+
+					setFtpValue([]);
+					// dispatch(sendFeed(ftpValuePass));
+				}
+
+				// console.log({ ftpValuePass })
 				setFtpValue([]);
-				dispatch(sendFeed(ftpValuePass, ftpParams.fTPSting));
+
 			}
 		});
 	};
 
-	const handleShape = (e, color, filterCut) => {
+	const handleColor = (e, shapLabelArray, cutLabelArray) => {
+		var getColorLabel = [];
+		for (let index = 0; index < e.length; index++) {
+			const element = e[index].label;
+			getColorLabel.push(element);
+		}
+
 		tableChangeHandler({
 			...table_data,
-			color: color.label != undefined && color.label != 'Reset' ? color.label : table_data.color,
-			shape: e.label == 'Reset' ? '' : e.label,
-			cut:
-				filterCut.label != undefined && filterCut.label != 'Reset'
-					? filterCut.label
-					: table_data.cut,
+			color__in: filterColor == [] ? '' : getColorLabel.join(','),
+			shape__in: shapLabelArray,
+			cut__in: cutLabelArray,
 		});
-		setFilterShape(e.label == 'Reset' ? 'Select Shape' : e);
+
+		setColorLabelArray(getColorLabel.join(','));
+		setFilterColor(e);
 	};
 
-	const handleCut = (e, color, filterShape) => {
+	const handleShape = (e, colorLabelArray, cutLabelArray) => {
+		var getShapLabel = [];
+		for (let index = 0; index < e.length; index++) {
+			const element = e[index].label;
+			getShapLabel.push(element);
+		}
+
 		tableChangeHandler({
 			...table_data,
-			color: color.label != undefined && color.label != 'Reset' ? color.label : table_data.color,
-			shape:
-				filterShape.label != undefined && filterShape.label != 'Reset'
-					? filterShape.label
-					: table_data.shape,
-			cut: e.label == 'Reset' ? '' : e.label,
+			color__in: colorLabelArray,
+			shape__in: filterShape == [] ? '' : getShapLabel.join(','),
+			cut__in: cutLabelArray,
 		});
-		setFilterCut(e.label == 'Reset' ? 'Select Cut' : e);
+
+		setShapeLabelArray(getShapLabel.join(''));
+		setFilterShape(e);
 	};
-	const handleColor = (e, filterCuts, filterShapes) => {
+
+	const handleCut = (e, colorLabelArray, shapLabelArray) => {
+		let getCutLabel = [];
+
+		for (let index = 0; index < e.length; index++) {
+			const element = e[index].label;
+			getCutLabel.push(element);
+		}
 		tableChangeHandler({
 			...table_data,
-			color: e.label == 'Reset' ? '' : e.label,
-			shape:
-				filterShapes.lable != undefined && filterShapes.label != 'Reset'
-					? filterShapes.lable
-					: table_data.shape,
-			cut:
-				filterCuts.label != undefined && filterCuts.label != 'Reset'
-					? filterCuts.label
-					: table_data.cut,
+			color__in: colorLabelArray,
+			shape__in: shapLabelArray,
+			cut__in: filterCut == [] ? '' : getCutLabel.join(','),
 		});
-		setFilterColor(e.label == 'Reset' ? 'Select Color' : e);
+		setCutLabelArray(getCutLabel.join(','));
+		setFilterCut(e);
 	};
 
 	const sendFeedClick = () => {
@@ -224,157 +273,123 @@ const FtpFeedList = () => {
 		});
 	};
 
+	const conditionalRowStyles = [
+		{
+			when: (row) => row.toggleSelected,
+			style: {
+				backgroundColor: 'green',
+				userSelect: 'none',
+				minHeight: '22px',
+				cursor: 'pointer',
+			},
+		},
+	];
+
+	const handleRowClicked = (row) => {
+		const updatedData = selectedData?.map((item) => {
+			if (row.id !== item.id) {
+				return item;
+			}
+
+			if (!item.toggleSelected == true) {
+				let getRecord = array?.find((ele) => ele.id === item.id);
+				if (!getRecord) {
+					setArray((oldArray) => [...oldArray, item]);
+				}
+			} else {
+				let getRecord = array?.find((ele) => ele.id === item.id);
+				setShowTable(false);
+				const index = array.indexOf(getRecord);
+				if (index > -1) {
+					array.splice(index, 1);
+				}
+			}
+			return {
+				...item,
+				toggleSelected: !item.toggleSelected,
+			};
+		});
+
+		setSelectedData(updatedData);
+		showTable && Addtolist();
+	};
+	const Addtolist = () => {
+		if (array.length >= 1) {
+			setShowTable(true);
+		} else {
+			setShowTable(false);
+		}
+	};
 	const dynamicHeight = Math.min(window.innerHeight * 4 + 1, 70) + 'vh';
 	return (
 		<Fragment>
-			{/* <Card> */}
-			{/* <CardHeader>
-					<CardTitle tag="h4">Search Filter</CardTitle>
-				</CardHeader> */}
-			{/* <CardBody>
-					<Form onSubmit={(e) => filterSubmit(e)}>
-						<Row>
-							<Col lg="3" md="6">
-								<Label for="color">FTP:</Label>
-								<Select
-									isClearable={false}
-									theme={selectThemeColors}
-									placeholder="Select FTP"
-									isMulti
-									name="ftp"
-									className="react-select"
-									classNamePrefix="select"
-									options={getAllDropdownValue}
-									value={ftpvalue}
-									onChange={(e) => handleChange(e)}
-								/>
-							</Col>
-							<Col lg="2" md="6">
-								<Label for="color">Color:</Label>
-								<Input id="color" name="color" placeholder="Enter Color" />
-							</Col>
-							<Col lg="2" md="6">
-								<Label for="shape">Shape:</Label>
-								<Input type="text" id="shape" name="shape" placeholder="Enter Shape" />
-							</Col>
-							<Col lg="2" md="6">
-								<Label for="cut">Cut:</Label>
-								<Input type="text" name="cut" placeholder="Enter Cut" />
-							</Col>
-							<Col lg="3" md="3">
-								<Label for="cut"></Label>
-								<Button.Ripple type="submit" color="relief-primary" block>
-									Filter
-								</Button.Ripple>
-							</Col>
-							<Col lg="2" md="3">
-								<Label for="send feed"></Label>
-								{ftpvalue && ftpvalue.length > 0 ? (
-									<Button.Ripple type="submit" color="relief-danger" onClick={openPopup} block>
-										Send Feed
-									</Button.Ripple>
-								) : (
-									<Button.Ripple type="submit" color="relief-danger" disabled={true} block>
-										Send Feed
-									</Button.Ripple>
-								)}
-							</Col>
-						</Row>
-					</Form>
-				</CardBody> */}
-			{/* </Card> */}
 			<Card className="deskboard_card">
 				<CardBody className="deskboard_card_body">
-					<Form onSubmit={(e) => filterSubmit(e)}>
-						<Row>
-							<Col lg="5" md="6">
-								<Select
-									size="sm"
-									isClearable={false}
-									theme={selectThemeColors}
-									placeholder="Select Client for FTP Feed"
-									isMulti
-									name="ftp"
-									className="react-select feed_select"
-									classNamePrefix="select"
-									options={getAllDropdownValue}
-									value={ftpvalue}
-									onChange={(e) => handleChange(e)}
-								/>
-							</Col>
-							<Col lg="2" md="6">
-								{/* <Label for="color">Color:</Label> */}
-								<Select
-									size="sm"
-									isClearable={false}
-									theme={selectThemeColors}
-									placeholder="Select Color"
-									className="react-select feed_select"
-									name="color"
-									classNamePrefix="select"
-									options={colorDropDownValue}
-									value={filterColor}
-									onChange={(e) => handleColor(e, filterCut, filterShape)}
-								/>
+					{/* <Form onSubmit={(e) => filterSubmit(e)}> */}
+					<Row>
+						<Col lg="3" md="6">
+							<Select
+								size="sm"
+								isClearable={false}
+								theme={selectThemeColors}
+								placeholder="Select Client for FTP Feed"
+								isMulti
+								name="ftp"
+								className="react-select feed_select"
+								classNamePrefix="select"
+								options={getAllDropdownValue}
+								value={ftpvalue}
+								onChange={(e) => handleChange(e)}
+							/>
+						</Col>
+						<Col lg="3" md="6">
+							{/* {/ <Label for="color">Color:</Label> /} */}
+							<Select
+								size="sm"
+								isClearable={false}
+								theme={selectThemeColors}
+								placeholder="Select Color"
+								className="react-select feed_select"
+								name="color"
+								isMulti
+								classNamePrefix="select"
+								options={colorDropDownValue}
+								value={filterColor}
+								onChange={(e) => handleColor(e, shapLabelArray, cutLabelArray)}
+							/>
+						</Col>
+						<Col lg="3" md="6">
+							<Select
+								size="sm"
+								isClearable={false}
+								theme={selectThemeColors}
+								placeholder="Select Shape"
+								name="shape"
+								className="react-select feed_select"
+								isMulti
+								classNamePrefix="select"
+								options={shapeDropDownValue}
+								value={filterShape}
+								onChange={(e) => handleShape(e, colorLabelArray, cutLabelArray)}
+							/>
+						</Col>
+						<Col lg="3" md="6">
+							<Select
+								size="sm"
+								isClearable={false}
+								theme={selectThemeColors}
+								placeholder="Select Cut"
+								name="cut"
+								className="react-select feed_select"
+								isMulti
+								classNamePrefix="select"
+								options={cutDropDownValue}
+								value={filterCut}
+								onChange={(e) => handleCut(e, colorLabelArray, shapLabelArray)}
+							/>
+						</Col>
 
-								{/* <Input
-									id="color"
-									name="color"
-									onChange={(e) => handleColor(e)}
-									size="sm"
-									placeholder="Enter Color"
-								/> */}
-							</Col>
-							<Col lg="2" md="6">
-								{/* <Label for="shape">Shape:</Label> */}
-								<Select
-									size="sm"
-									isClearable={false}
-									theme={selectThemeColors}
-									placeholder="Select Shape"
-									name="shape"
-									className="react-select feed_select"
-									classNamePrefix="select"
-									options={shapeDropDownValue}
-									value={filterShape}
-									onChange={(e) => handleShape(e, filterColor, filterCut)}
-								/>
-								{/* <Input
-									type="text"
-									id="shape"
-									size="sm"
-									onChange={(e) => handleShape(e)}
-									name="shape"
-									placeholder="Enter Shape"
-								/> */}
-							</Col>
-							<Col lg="2" md="6">
-								{/* <Label for="cut">Cut:</Label> */}
-
-								<Select
-									size="sm"
-									isClearable={false}
-									theme={selectThemeColors}
-									placeholder="Select Cut"
-									name="cut"
-									className="react-select feed_select"
-									classNamePrefix="select"
-									options={cutDropDownValue}
-									onChange={(e) => handleCut(e, filterColor, filterShape)}
-									value={filterCut}
-									// onChange={(e) => handleChange(e)}
-								/>
-
-								{/* <Input
-									type="text"
-									size="sm"
-									name="cut"
-									onChange={(e) => handleCut(e)}
-									placeholder="Enter Cut"
-								/> */}
-							</Col>
-
-							{/* <Col lg="1" md="3">
+						{/* <Col lg="1" md="3">
 								
 								<Button.Ripple
 									type="submit"
@@ -386,58 +401,100 @@ const FtpFeedList = () => {
 									Filter
 								</Button.Ripple>
 							</Col> */}
-							<Col lg="1" md="3" className="pl-0">
-								{/* <Label for="send feed"></Label> */}
-								{ftpvalue && ftpvalue.length > 0 && ftpFeedData?.results?.length > 0 ? (
-									<Button.Ripple
-										type="submit"
-										size="sm"
-										color="relief-danger"
-										onClick={openPopup}
-										className="seed_button"
-										block
-									>
-										Send Feed
-									</Button.Ripple>
-								) : (
-									<Button.Ripple
-										type="submit"
-										size="sm"
-										color="relief-danger"
-										onClick={
-											ftpFeedData?.results?.length == 0 ? () => sendFilter() : () => sendFeedClick()
-										}
-										style={{ opacity: '0.6' }}
-										className="seed_button"
-										block
-									>
-										Send Feed
-									</Button.Ripple>
-								)}
-							</Col>
-						</Row>
-					</Form>
+					</Row>
+
+					<Row>
+						<Col xl="4 mt-2">
+							{array?.length > 0 ? (
+								<Button.Ripple
+									size="sm"
+									onClick={() => Addtolist()}
+									className="btn-success"
+									style={{ cursor: 'pointer' }}
+								>
+									Add to list
+								</Button.Ripple>
+							) : null}
+						</Col>
+					</Row>
 				</CardBody>
 				<DataTable
 					noHeader
 					pagination
 					// subHeader
+					selectableRows
 					responsive
 					paginationServer
 					columns={columns}
-					data={ftpFeedData.results}
-					paginationTotalRows={ftpFeedData.count}
+					// data={ftpFeedData?.results}
+					data={selectedData}
+					onRowClicked={handleRowClicked}
+					paginationTotalRows={ftpFeedData?.count}
 					paginationRowsPerPageOptions={datatable_per_raw}
 					onChangeRowsPerPage={handlePerRowsChange}
 					onChangePage={handlePageChange}
+					conditionalRowStyles={conditionalRowStyles}
 					sortIcon={<ChevronDown />}
 					className="react-dataTable"
 					fixedHeader
 					fixedHeaderScrollHeight={dynamicHeight}
 					paginationPerPage={table_data.per_page}
-					// progressPending={ftpFeedData.length == 0 ? true : false}
+				// progressPending={ftpFeedData.length == 0 ? true : false}
 				/>
 			</Card>
+
+			<Col lg="1" md="3" className="pl-0 mt-2">
+				{/* <Label for="send feed"></Label> */}
+				{ftpvalue && ftpvalue.length > 0 && ftpFeedData?.results?.length > 0 ? (
+					<Button.Ripple
+						type="submit"
+						size="sm"
+						color="relief-danger"
+						onClick={openPopup}
+						className="seed_button"
+						block
+					>
+						Send Feed
+					</Button.Ripple>
+				) : (
+					<Button.Ripple
+						type="submit"
+						size="sm"
+						color="relief-danger"
+						onClick={ftpFeedData?.results?.length == 0 ? () => sendFilter() : () => sendFeedClick()}
+						style={{ opacity: '0.6' }}
+						className="seed_button"
+						block
+					>
+						Send Feed
+					</Button.Ripple>
+				)}
+			</Col>
+			{showTable && (
+				<div className="mt-4">
+					{/* <Card className="deskboard_card"> */}
+					<DataTable
+						noHeader
+						// pagination
+						// subHeader
+						responsive
+						// paginationServer
+						columns={columns}
+						// data={ftpFeedData?.results}
+						data={array}
+						// paginationRowsPerPageOptions={datatable_per_raw}
+						// onChangeRowsPerPage={handlePerRowsChange}
+						// onChangePage={handlePageChange}
+						// sortIcon={<ChevronDown />}
+						className="react-dataTable"
+						fixedHeader
+						fixedHeaderScrollHeight={dynamicHeight}
+					// paginationPerPage={table_data.per_page}
+					// progressPending={ftpFeedData.length == 0 ? true : false}
+					/>
+					{/* </Card> */}
+				</div>
+			)}
 		</Fragment>
 	);
 };
